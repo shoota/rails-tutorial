3fb91baf93af28186184b7546bf4194b6b5f04d8
shoota <shoota.dev@gmail.com>
2014-06-18 22:55:46 +0900

    3.3.1 - 3.3.2 application.html.erbを移動して 静的ページのタイトルをベタ書きするテスト。2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,18 +7,31 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
+
   describe "Help page" do
   	it "should have the content 'Help' " do
   	  visit '/static_pages/help'
   	  expect(page).to have_content('Help')
   	end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
   end
+
   describe "About page" do
     it "should have the content 'About Us' " do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
-
 end
