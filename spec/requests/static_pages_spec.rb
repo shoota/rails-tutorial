73764b769556ddc3a1cc9c38751495f69a8a2185
shoota <shoota.dev@gmail.com>
2014-06-23 00:00:08 +0900

    4.1 helperを使ってtitleに追加文言が条件付きで設定される。 - 追加のページ名を与えるとtitleに追加するhelper - viewをhelper methodに対応させ、homeは追加文言がでなくなる - specで否定条件のテストを行う

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
-describe "StaticPages" do	
-
+describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App' " do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
